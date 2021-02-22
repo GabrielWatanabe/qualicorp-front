2ab19c3e51ab91abe7ee592514c29814b8ac1fa5
Gabriel Watanabe <gabriel.yujip@gmail.com>
2021-02-22 00:05:44 -0300

    alterado a variavel de ambiente base url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://qualicorp-back.herokuapp.com/",
+  baseURL: "https://qualicorp-back.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*"
